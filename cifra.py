b7cea6ba3bdf3af72156e2be94174c3dfaf77f59
Lívia Chaves Maia <31578473+LiviaChaves@users.noreply.github.com>
2022-09-19 17:51:52 -0300

    Add files via upload

--- cifra.py
+++ cifra.py
@@ -1,26 +1,31 @@
-ENCRYPT = 1
-DECRYPT = 0
 
 
-key = int(input('Digite o valor da chave até 76: '))
 
-frase = input('Digite a frase: ')
 
-alphabet = 'abcdefghijklmnopqrstuvwyzàáãâéêóôõíúçABCDEFGHIJKLMNOPQRSTUVWYZÀÁÃÂÉÊÓÕÍÚÇ'
+
+mode = int(input('Digite o 1 para ENCRYPT ou 0 para DECRYPT: ')) #recebe o modo
+
+key = int(input('Digite o valor da chave até 76: '))  #recebe a chave
+
+frase = input('Digite a frase: ')  #recebe a frase
+
+alphabet = 'abcdefghijklmnopqrstuvwyzàáãâéêóôõíúçABCDEFGHIJKLMNOPQRSTUVWYZÀÁÃÂÉÊÓÕÍÚÇ'  #alfabeto definido com letras maiúsculas e minusculas acentuadas
 
 def caesar(data, key, mode):
     new_data = ''
     for caractere  in data:
-        index = alphabet.find(caractere)
+        index = alphabet.find(caractere)  #caractere variavel definida para percorrer o alfabeto e selecionar a letra
         if index == -1:
             new_data += caractere 
         else:
-            new_index = index + key if mode == ENCRYPT else index - key
+            new_index = index + key if mode == 1 else index - key #nova letra recebe a letra atual + a chave se o modo for o de encriptografar se não ira descriptografar utilizando a subtração da nova letra com a chave selecionada.
             new_index = new_index % len(alphabet)
             new_data += alphabet[new_index:new_index+1]
     return new_data
-
-ciphered = caesar(frase, key, ENCRYPT)
-print('Encriptada:', ciphered)
-plain = caesar(ciphered, key, DECRYPT)
-print('Decriptada:', plain) 
+    
+if mode == 1:
+    e = caesar(frase, key, 1)
+    print('Encriptada:', e)
+if mode == 0:
+    d = caesar(frase, key, 0)
+    print('Decriptada:', d)
